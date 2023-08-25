f43e1c3d4950e3122c03d6826b6fff061ca27f9a
Luke Ramljak <lukeramljak@proton.me>
2023-08-25 23:33:13 +0800

    add hover effect to grid

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const container = document.querySelector('.container');
 
 createGrid(8);
 
+container.addEventListener('mouseover', (event) => {
+  if (event.target.classList.contains('square')) {
+    event.target.classList.add('square-hover');
+  }
+});
+
+container.addEventListener('mouseout', (event) => {
+  if (event.target.classList.contains('square')) {
+    event.target.classList.remove('square-hover');
+  }
+});
+
 function createGrid(size) {
   container.style.gridTemplateColumns = `repeat(${size}, 24px)`;
   container.style.gridTemplateRows = `repeat(${size}, 24px)`;
@@ -10,7 +22,8 @@ function createGrid(size) {
 
   for (let i = 0; i < size * size; i++) {
     let square = document.createElement('div');
-    squares.appendChild(square).className = 'square';
+    square.classList.add('square');
+    squares.appendChild(square);
   }
   container.appendChild(squares);
 }
